486d07158057769fc2722f2e1b770d38c4b073a4
Dylan McAulay <dmcaulay97@gmail.com>
2021-06-03 11:25:09 -0400

    changed server location in file structure and changed all paths in server file

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(__dirname + "/../public"));
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/../public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
 
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/../public/notes.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
 
 app.get("/api/notes", (req, res) => {
-    fs.readFile("../db/db.json", (error, data) => {
+    fs.readFile("db/db.json", (error, data) => {
         if (error) {
             console.error(error)
         } else {
@@ -27,7 +27,7 @@ app.get("/api/notes", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
-    fs.readFile("../db/db.json", (error, data) => {
+    fs.readFile("db/db.json", (error, data) => {
         if (error) {
             console.error(error)
         } else {
@@ -36,7 +36,7 @@ app.post("/api/notes", (req, res) => {
             let newNote = req.body;
             newNote["id"] = uniqid();
             notes.push(newNote);
-            fs.writeFile("../db/db.json", JSON.stringify(notes), (err) =>
+            fs.writeFile("db/db.json", JSON.stringify(notes), (err) =>
                 err ? console.error(err) : console.log('New note has been stored in db'))
             res.json(newNote);
         }
@@ -44,13 +44,13 @@ app.post("/api/notes", (req, res) => {
 })
 
 app.delete("/api/notes/:id", (req, res) => {
-    fs.readFile("../db/db.json", (error, data) => {
+    fs.readFile("db/db.json", (error, data) => {
         if (error) {
             console.error(error);
         } else {
             let notes = JSON.parse(data);
             notes = notes.filter(e => e.id != req.params.id)
-            fs.writeFile("../db/db.json", JSON.stringify(notes), (err) =>
+            fs.writeFile("db/db.json", JSON.stringify(notes), (err) =>
                 err ? console.error(err) : console.log('Note has been deleted'))
             res.send({});
         }
